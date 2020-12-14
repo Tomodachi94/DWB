1ab7c2a31c96a5b7d496d0f8d0057619b9c5874d
Tomodachi94 <68489118+Tomodachi94@users.noreply.github.com>
2020-12-14 01:00:25 -0800

    Create bot.py, dir

---
+++ src/bot.py
@@ -0,0 +1,11 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
