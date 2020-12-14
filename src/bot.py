8d2072e7f2d423c0038f0336247d727f2face81f
Tomodachi94 <68489118+Tomodachi94@users.noreply.github.com>
2020-12-14 12:50:57 -0800

    Redid that mess-up.

--- src/bot.py
+++ src/bot.py
@@ -9,3 +9,31 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
+
+from discord.ext import commands
+prefix = "_"
+bot = commands.Bot(command_prefix=prefix)
+
+
+@bot.event
+async def on_ready():
+    print("Dash is online and awaiting a command...~")
+
+@bot.command()
+async def ping(ctx):
+    '''
+    This text will be shown in the help command
+    '''
+
+    # Get the latency of the bot
+    latency = bot.latency  # Included in the Discord.py library
+    # Send it to the user
+    await ctx.send(latency)
+
+
+@bot.command()
+async def echo(ctx, *, content:str):
+    await ctx.send(content)
+
+
+bot.run(TOKEN)  # Where 'TOKEN' is your bot token
