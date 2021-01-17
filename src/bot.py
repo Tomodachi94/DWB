e777a14e2d9b24ac3ece7a516c7b5102a61409cd
Tomodachi94 <tomodachi94@protonmail.com>
2021-01-16 21:27:19 -0800

    Remove "formatter" for logging module
    Breaks for some reason, I'll have a look at this later

--- src/bot.py
+++ src/bot.py
@@ -8,10 +8,6 @@
 
 import logging
 
-logging.basicConfig(
-	formatter = logging.Formatter(
-		"%(asctime)s - %(name)s - %(levelname)s %(pathname)s - %(message)s"))
-
 logging.info("Starting Dash...")
 logging.info("Loading dependencies...")
 
