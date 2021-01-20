5cd00cac388c0dc39820dc3f1664522837e4c8fa
Tomodachi94 <tomodachi94@protonmail.com>
2021-01-19 18:29:59 -0800

    flake8-ified

--- src/badwords.py
+++ src/badwords.py
@@ -3,9 +3,9 @@
 # TOMODACHI94 WAIVES ALL LIABILITY FOR YOU READING THIS
 
 insults = ["bitch",
-    "annoyance",
-    "fucking fucker",
-    "dicsord", "jerkwad",
-    "something that needs to go to the Nether",
-    "[REDACTED]", "cotton-headed ninnymuggins",
-    "something that needs to commit Lego-step", ]
+            "annoyance",
+            "fucking fucker",
+            "dicsord", "jerkwad",
+            "something that needs to go to the Nether",
+            "[REDACTED]", "cotton-headed ninnymuggins",
+            "something that needs to commit Lego-step", ]
